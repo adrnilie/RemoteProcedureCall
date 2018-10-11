56f3a94abc849dfa7820e6f33074ef1fb626bd26
Adrian <sci.adrian@gmail.com>
2018-10-11 14:33:45 +0300

    Remote Procedure Call initial commit

---
+++ RemoteProcedureCall/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteProcedureCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var ipAddress = IPAddress.Parse("192.168.100.8");
+                var myList = new TcpListener(ipAddress, 4444);
+                myList.Start();
+
+                Console.WriteLine("Remote Procedure Call SERVER");
+                Console.WriteLine($"Server is runing on {ipAddress}:4444");
+                Console.WriteLine($"Local Endpoint: {myList.LocalEndpoint}");
+                Console.WriteLine($"Waiting for connections...");
+
+                var socket = myList.AcceptSocket();
+                Console.WriteLine($"Connection Accepted from : {socket.RemoteEndPoint}");
+
+                var b = new byte[100];
+                var k = socket.Receive(b);
+
+                Console.WriteLine("Recieved..");
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(b[i]));
+                }
+                var asencd = new ASCIIEncoding();
+                socket.Send(asencd.GetBytes("Automatic Message:" + "String Received byte server !"));
+                Console.WriteLine("\nAutomatic Message is Sent");
+                socket.Close();
+                myList.Stop();
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error.." + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ RemoteProcedureCallClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteProcedureCallClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Tcpclient = new TcpClient();
+
+                Console.WriteLine("Connecting...");
+                Tcpclient.Connect("192.168.100.8", 4444);
+                Console.WriteLine("Connected");
+                Console.WriteLine("Ente the String you want to send ");
+
+                var str = Console.ReadLine();
+                var stm = Tcpclient.GetStream();
+                var ascnd = new ASCIIEncoding();
+                var ba = ascnd.GetBytes(str);
+
+                Console.WriteLine("Sending..");
+                stm.Write(ba, 0, ba.Length);
+
+                var bb = new byte[100];
+                var k = stm.Read(bb, 0, 100);
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bb[i]));
+                }
+
+                Tcpclient.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.StackTrace);
+            }
+        }
+    }
+}
